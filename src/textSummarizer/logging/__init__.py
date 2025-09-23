547a2d12c686fa07123bd6515d07ccba0bab5b6d
gokilanr <gokilanraja1428@gmail.com>
2025-09-23 09:16:33 +0530

    updated common utils

--- src/textSummarizer/logging/__init__.py
+++ src/textSummarizer/logging/__init__.py
@@ -5,7 +5,7 @@ import logging
 log_dir="logs"
 logging_str = "[%(asctime)s: %(levelname)s:%(module)s: %(message)s]"
 
-log_filepath = os.path.join(log_dir, "continuos_log.log")
+log_filepath = os.path.join(log_dir, "continuos_logs.log")
 
 os.makedirs(log_dir, exist_ok=True)
 
